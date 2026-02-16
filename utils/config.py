e2805aa0a9a700c5e775277ffca7499bef771b9e
yakhoob32 <yakhoob32@gmail.com>
2026-02-16 22:56:01 +0330

    utils

---
+++ utils/config.py
@@ -0,0 +1,28 @@
+"""Start with 6 for testing Phase 1, change to 12 for the final Elite version."""
+BOARD_SIZE = 6
+
+""" Maximum time (in seconds) a player has to make a move."""
+TURN_TIME_LIMIT = 30
+
+"""How many turns must pass before the 8x8 cloud event is triggered."""
+CLOUD_TRIGGER_INTERVAL = 8
+"""Duration (in turns) the cloud stays on the board."""
+CLOUD_DURATION = 3
+"""Dimension of the storm cloud (3x3)."""
+CLOUD_SIZE = 3
+
+"""These will be used to populate the army in the setup phase."""
+ARMY_COMPOSITION = {
+    'MARSHAL' : 1,
+    'GENERAL' : 1,
+    'COLONEL' : 2,
+    'MAJOR' : 3,
+    'CAPTAIN' : 4,
+    'LIEUTENANT' : 4,
+    'SERGEANT' : 4,
+    'MINER' : 5,
+    'SCOUT' : 8,
+    'SPY' : 1,
+    'BOMB' : 6,
+    'FLAG' : 1
+}
---
+++ utils/constants.py
@@ -0,0 +1,61 @@
+from enum import Enum, auto
+
+class Team(Enum) :
+    """Represents the two competing sides and an empty state."""
+    RED = auto()
+    BLUE = auto()
+    NONE = auto()
+
+class PieceRank(Enum) :
+    """Defines all possible ranks in the game, including special units."""
+    MARSHAL = 10
+    GENERAL = 9
+    COLONEL = 8
+    MAJOR = 7
+    CAPITAIN = 6
+    LIEUTENANT = 5
+    SERGEANT = 4
+    MINER = 3
+    SCOUT = 2
+    SPY = 'S'
+    BOMB = 'B'
+    FLAG = 'F'
+    UNKNOWN = '?'
+
+class Command(Enum) :
+    """Standardized commands for Client-Server communication and UI events."""
+    CONNECT = auto()
+    START_GAME = auto()
+    SETUP_DONE = auto()
+
+    MOVE = auto()
+    ATTACK = auto()
+    USE_POWER = auto()
+
+    UPDATE_BOARD = auto()
+    BATTLE_RESULT = auto()
+    CLOUD_EVENT = auto()
+    TURN_TIMEOUT = auto()
+    GAME_OVER = auto()
+
+class PowerType(Enum) :
+    """Specific types of special abilities (Power Tokens)."""
+    RADAR = auto()
+    REPAIR = auto()
+    MINE_ADD = auto()
+    SCOUT_REVEAL = auto()
+    DOUBLE_MOVE = auto()
+
+class CellType(Enum) :
+    """Categorizes the nature of board tiles for movement and visibility."""
+    EMPTY = auto()
+    LAKE = auto()
+    CLOUD = auto()
+
+class GameState(Enum) :
+    """Tracks the current state of the global game loop."""
+    WAITING_FOR_PLAYERS = auto()
+    SETUP_PHASE = auto()
+    IN_PROGRESS = auto()
+    FOG_STORM = auto()
+    FINISHED = auto()
